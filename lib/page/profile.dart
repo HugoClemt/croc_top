80c548efb7f097d8d006de0e2ebbc5b025e49780
HugoClemt <hugoclemt@gmail.com>
2024-03-01 20:43:23 +0100

    Grille de bouton pour faire les catégorie de recette ajoute ( manque image + style )

--- lib/page/profile.dart
+++ lib/page/profile.dart
@@ -1,5 +1,6 @@
 import 'package:croc_top/page/home.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -11,81 +12,110 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   int _currentIndex = 3;
 
+  final List<String> imagePaths = [
+    'assets/icon-apero.svg',
+    'assets/icon-salad.svg',
+    'assets/icon-dishes.svg',
+    'assets/icon-desserts.svg',
+    'assets/icon-sauce.svg',
+    'assets/icon-cocktail.svg',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox(
-        height: 500,
-        child: Column(
-          children: [
-            Expanded(
-              child: Container(
-                color: Colors.grey[300],
-                child: Row(
-                  children: [
-                    SizedBox(
-                      width: 175,
-                      child: Expanded(
-                        child: Container(
-                          child: const Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              CircleAvatar(
-                                radius: 50,
-                                backgroundImage:
-                                    AssetImage('assets/profile_image.jpg'),
+      body: Column(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.25,
+            child: Container(
+              color: Colors.grey[300],
+              child: Row(
+                children: [
+                  const SizedBox(
+                    width: 155,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        CircleAvatar(
+                          radius: 50,
+                          backgroundImage: AssetImage('assets/profile.png'),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      padding: const EdgeInsets.only(top: 100),
+                      child: const Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            child: Center(
+                              child: Row(
+                                children: [
+                                  Text('HugoClemt'),
+                                ],
                               ),
-                            ],
+                            ),
+                          ),
+                          SizedBox(height: 20),
+                          SizedBox(
+                            child: Center(
+                              child: Row(
+                                children: [
+                                  SizedBox(
+                                    child: Text('Abonnement : 15'),
+                                  ),
+                                  SizedBox(width: 15),
+                                  SizedBox(
+                                    child: Text('Abonnées : 982'),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: GridView.count(
+              crossAxisCount: 3,
+              children: List.generate(
+                6,
+                (index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        print('Button $index pressed');
+                      },
+                      style: ButtonStyle(
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(0.0),
                           ),
                         ),
                       ),
-                    ),
-                    Expanded(
-                      child: Container(
-                        padding: const EdgeInsets.only(top: 100),
-                        child: const Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            SizedBox(
-                              child: Center(
-                                child: Row(
-                                  children: [
-                                    Text('HugoClemt'),
-                                  ],
-                                ),
-                              ),
-                            ),
-                            SizedBox(height: 20),
-                            SizedBox(
-                              child: Center(
-                                child: Row(
-                                  children: [
-                                    SizedBox(
-                                      child: Text('Abonnement : 15'),
-                                    ),
-                                    SizedBox(width: 15),
-                                    SizedBox(
-                                      child: Text('Abonnées : 150'),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
+                      child: SvgPicture.asset(
+                        imagePaths[index],
+                        width: 50,
+                        height: 50,
+                        color: Colors.black,
                       ),
                     ),
-                  ],
-                ),
+                  );
+                },
               ),
             ),
-            Expanded(
-              child: Center(
-                child: Text('Selected Index: $_currentIndex'),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
